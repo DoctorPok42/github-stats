f3777752ec3351c4769fb6410e6d1738396fcce6
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2022-04-27 14:12:10 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import React, { useState } from "react";
+
+export default function Home() {
+
+  const [usersvalue, userssetValue] = useState('');
+
+  return (
+    <main className="container">
+      <Head>
+        <title>GitHub Stats</title>
+        <meta charSet="UTF8" />
+        <meta name="theme-color" content="#d36e18" />
+        <meta name="title" content="GitHub Stats" />
+        <meta name="description" content="Application Web d'information"/>
+        <meta name="author" content="DoctorPok" />
+        <meta name="robots" content="index" />
+        <script
+          src="https://kit.fontawesome.com/73458f3ed9.js"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://kit.fontawesome.com/b028783a83.js"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+
+      <div id="search">
+      <input type="text" className="txt" placeholder={"Search a GitHub user"} value={usersvalue} onChange={(e) => {
+              userssetValue(e.target.value)
+      }}/>
+      <button className="search-button" onClick={() => {
+          window.location.href = `/user/${usersvalue}`
+        }}>
+      </button>
+      </div>
+    </main>
+  )
+}
